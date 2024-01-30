123cbd751aac94be41c490fe3c5c67c865aefdef
Gordon <gordonchen2014@gmail.com>
2024-01-30 18:07:18 -0500

    drop sqlc, use raw pgx

--- postgresql/leaguev4.go
+++ postgresql/leaguev4.go
@@ -3,43 +3,8 @@ package postgresql
 import (
 	"context"
 	"time"
-)
 
-const (
-	insertSoloqRecord = `
-	INSERT INTO soloq_records
-	(record_date, league_id, summoner_name, tier, rank, league, wins, losses)
-	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
-	RETURNING record_id
-	`
-
-	countSoloqRecordsById = `
-	SELECT COUNT(*)
-	FROM soloq_records
-	WHERE summoner_id = $1
-	`
-
-	deleteSoloqRecord = `
-	DELETE FROM soloq_records
-	WHERE record_id = $1
-	RETURNING record_date, summoner_name
-	`
-
-	countSoloqRecordsBySummonerId = `
-	SELECT *
-	FROM soloq_records
-	WHERE summoner_id = $1
-	ORDER BY record_date DESC
-	LIMIT $2 OFFSET $3;
-	`
-
-	coutnSoloqRecordsByName = `
-	SELECT *
-	FROM soloq_records
-	WHERE summoner_name = $1
-	ORDER BY record_date DESC
-	LIMIT $2 OFFSET $3;
-	`
+	"github.com/jackc/pgx/v5"
 )
 
 type SoloqRecord struct {
@@ -51,18 +16,97 @@ type SoloqRecord struct {
 	Tier         string
 	Rank         string
 	League       string
-	wins         int32
-	losses       int32
+	Wins         int32
+	Losses       int32
 }
 
-func InsertSoloqRecord(ctx context.Context, r *SoloqRecord) (string, error) {
-	return "", nil
+func (q *Queries) InsertSoloqRecord(ctx context.Context, r *SoloqRecord) (string, error) {
+	var id string
+
+	query := `
+	INSERT INTO soloq_records
+	(record_date, league_id, summoner_name, tier, rank, league, wins, losses)
+	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+	RETURNING record_id
+	`
+
+	err := q.db.QueryRow(ctx, query,
+		r.RecordDate,
+		r.LeagueId,
+		r.SummonerName,
+		r.Tier,
+		r.Rank,
+		r.League,
+		r.Wins,
+		r.Losses,
+	).Scan(&id)
+
+	return id, err
 }
 
-func CountSoloqRecordsById(ctx context.Context, id string) (int64, error) {
-	return 0, nil
+func (q *Queries) SelectSoloqRecordsBySummonerId(ctx context.Context, puuid string) ([]SoloqRecord, error) {
+	query := `
+	SELECT *
+	FROM soloq_records
+	WHERE summoner_id = $1
+	ORDER BY record_date DESC
+	LIMIT $2 OFFSET $3;
+	`
+	rows, _ := q.db.Query(ctx, query, puuid)
+	defer rows.Close()
+
+	records, err := pgx.CollectRows(rows, pgx.RowToStructByName[SoloqRecord])
+	return records, err
 }
 
-func DeletSoloqRecord(ctx context.Context, id string) (string, time.Time, error) {
-	return "", time.Time{}, nil
+func (q *Queries) SelectSoloqRecord(ctx context.Context, id string) (SoloqRecord, error) {
+	query := `
+	SELECT * 
+	`
+
+	rows, _ := q.db.Query(ctx, query, id)
+	defer rows.Close()
+
+	record, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[SoloqRecord])
+	return record, err
+}
+
+func (q *Queries) CountSoloqRecordsBySummonerId(ctx context.Context, id string) (int64, error) {
+	query := `
+	SELECT COUNT(*)
+	FROM soloq_records
+	WHERE summoner_id = $1
+	`
+
+	var count int64
+	err := q.db.QueryRow(ctx, query, id).Scan(&count)
+	return count, err
+}
+
+func (q *Queries) DeletSoloqRecord(ctx context.Context, id string) (string, time.Time, error) {
+	query := `
+	DELETE FROM soloq_records
+	WHERE record_id = $1
+	RETURNING record_date, summoner_name
+	`
+	var s string
+	var t time.Time
+	err := q.db.QueryRow(ctx, query, id).Scan(&s, &t)
+	return s, t, err
+}
+
+func (q *Queries) SelectSoloqRecordsByName(ctx context.Context, name string) ([]SoloqRecord, error) {
+	query := `
+	SELECT *
+	FROM soloq_records
+	WHERE summoner_name = $1
+	ORDER BY record_date DESC
+	LIMIT $2 OFFSET $3;
+	`
+
+	rows, _ := q.db.Query(ctx, query)
+	defer rows.Close()
+
+	records, err := pgx.CollectRows(rows, pgx.RowToStructByName[SoloqRecord])
+	return records, err
 }
--- postgresql/summonerv4.go
+++ postgresql/summonerv4.go
@@ -18,44 +18,16 @@ type SummonerRecord struct {
 	SummonerLevel int64
 }
 
-const (
-	insertSummonerRecord = `
+func (q *Queries) InsertSummonerRecord(ctx context.Context, rec *SummonerRecord) (string, error) {
+	query := `
 	INSERT INTO summoner_records
 	(record_date, account_id, profile_icon_id, revision_date, name, id, puuid, summoner_level)
 	VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
 	RETURNING record_id
 	`
 
-	selectSummonerRecordById = `
-	SELECT *
-	FROM summoner_records
-	WHERE id = $1
-	`
-
-	deleteSummonerRecord = `
-	DELETE FROM summoner_records
-	WHERE record_id = $1
-	RETURNING record_date, name
-	`
-
-	countSummonerRecordsByPuuid = `
-	SELECT COUNT(*)
-	FROM summoner_records
-	WHERE puuid = $1
-	`
-
-	selectSummonerRecordsByPuuid = `
-	SELECT *
-	FROM summoner_records
-	WHERE puuid = $1
-	ORDER BY record_date DESC
-	LIMIT $2 OFFSET $3
-	`
-)
-
-func (q *Queries) InsertSummonerRecord(ctx context.Context, rec *SummonerRecord) (string, error) {
 	var id string
-	err := q.db.QueryRow(ctx, insertSummonerRecord,
+	err := q.db.QueryRow(ctx, query,
 		rec.RecordDate,
 		rec.AccountId,
 		rec.ProfileIconId,
@@ -70,7 +42,13 @@ func (q *Queries) InsertSummonerRecord(ctx context.Context, rec *SummonerRecord)
 }
 
 func (q *Queries) SelectSummonerRecordByPuuid(ctx context.Context, puuid string) ([]SummonerRecord, error) {
-	rows, _ := q.db.Query(ctx, selectSummonerRecordsByPuuid, puuid)
+	query := `
+	SELECT *
+	FROM summoner_records
+	WHERE id = $1
+	`
+
+	rows, _ := q.db.Query(ctx, query, puuid)
 	defer rows.Close()
 
 	records, err := pgx.CollectRows(rows, pgx.RowToStructByName[SummonerRecord])
@@ -82,7 +60,15 @@ func (q *Queries) SelectSummonerRecordByPuuid(ctx context.Context, puuid string)
 }
 
 func (q *Queries) SelectSummonerRecordById(ctx context.Context, id string) (SummonerRecord, error) {
-	rows, _ := q.db.Query(ctx, selectSummonerRecordById, id)
+	query := `
+	SELECT *
+	FROM summoner_records
+	WHERE puuid = $1
+	ORDER BY record_date DESC
+	LIMIT $2 OFFSET $3
+	`
+
+	rows, _ := q.db.Query(ctx, query, id)
 	defer rows.Close()
 
 	record, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[SummonerRecord])
@@ -90,14 +76,26 @@ func (q *Queries) SelectSummonerRecordById(ctx context.Context, id string) (Summ
 }
 
 func (q *Queries) CountSummonerRecordsByPuuid(ctx context.Context, puuid string) (int64, error) {
+	query := `
+	SELECT COUNT(*)
+	FROM summoner_records
+	WHERE puuid = $1
+	`
+
 	var count int64
-	err := q.db.QueryRow(ctx, countSummonerRecordsByPuuid, puuid).Scan(&count)
+	err := q.db.QueryRow(ctx, query, puuid).Scan(&count)
 	return count, err
 }
 
 func (q *Queries) DeleteSummonerRecord(ctx context.Context, id string) (string, time.Time, error) {
+	query := `
+	DELETE FROM summoner_records
+	WHERE record_id = $1
+	RETURNING record_date, name
+	`
+
 	var s string
 	var t time.Time
-	err := q.db.QueryRow(ctx, deleteSummonerRecord, id).Scan(&s, &t)
+	err := q.db.QueryRow(ctx, query, id).Scan(&s, &t)
 	return s, t, err
 }
